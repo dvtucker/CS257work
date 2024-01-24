830ee3742823dea5ef55e030b726381352db33b2
dayatucker <dvtucker04@gmail.com>
2024-01-24 09:10:35 -0600

    add states and us cities datasets

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,1 +1,41 @@
+import psycopg2
+from config import config
 
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        
+        CREATE TABLE cities (
+            vendor_id SERIAL PRIMARY KEY,
+            vendor_name VARCHAR(255) NOT NULL
+        )
+        ,
+         CREATE TABLE stateAbb (
+                part_id SERIAL PRIMARY KEY,
+                part_name VARCHAR(255) NOT NULL
+                )
+        )
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
