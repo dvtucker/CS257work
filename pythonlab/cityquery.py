decea64a04e0170128196cd834d70ad713ac4d33
dayatucker <dvtucker04@gmail.com>
2024-02-01 13:09:50 -0600

    modified cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -118,7 +118,7 @@ def test_query_five():
     if len(state_name) == 2:
         state_name = state_name.upper()  # Convert to uppercase 
         cur.execute("SELECT stateName FROM stateAbb WHERE abbreviation='" + state_name + "';")
-        state_name = cur.fetchone()[1]
+        state_name = cur.fetchone()
 
     # get the city population for the specified state
     cur.execute("SELECT cityPopulation FROM cities WHERE stateName='" + state_name + "';")
