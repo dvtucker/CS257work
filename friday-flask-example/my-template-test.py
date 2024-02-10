fbc1cda0b2e76d0a8eade9a57c44e9bc270d47de
dayatucker <dvtucker04@gmail.com>
2024-02-10 14:02:02 -0600

    updates to all filesl

--- friday-flask-example/my-template-test.py
+++ friday-flask-example/my-template-test.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import random
+import psycopg2
 
 app = Flask(__name__)
 
@@ -8,15 +9,22 @@ app = Flask(__name__)
 def welcome():
     return render_template("index2.html")
 
-@app.route('/rand/<low>/<high>')
-def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
-    low_int = int(low)
-    high_int = int(high)
+@app.route('/getName')
+def getName():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="tuckerd",
+        user="tuckerd",
+        password="carpet664winter")
     
-    num = random.randint(low_int, high_int)
-    return render_template("random2.html", randNum = num)
+    cur = conn.cursor()
+    sql = "SELECT person_name FROM names_adjectives_table"
+    cur.execute( sql )
+    result = cur.fetchall()
+    rand_name = result(random.randint(0,7))
+    rand_year = random.randint(1800, 2024)
+    return render_template("random2.html", name = rand_name, year = rand_year)
 
 if __name__ == '__main__':
     my_port = 5136
