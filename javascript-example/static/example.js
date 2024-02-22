50d314c67ff58929e929fc042a003924d3523b42
dayatucker <dvtucker04@gmail.com>
2024-02-21 18:52:18 -0600

    added example files

---
+++ javascript-example/static/example.js
@@ -0,0 +1,9 @@
+function changeColor() {
+  the_heading = document.getElementById("hello");
+  the_heading.style.color = "red";
+}
+
+heading = document.getElementById("hello");
+heading.onmouseover = function(){
+  heading.fontFamily = "Times New Roman";
+}
